62b5ff4ace29a4c5367d5f3aed95729a579922fd
Maksym-Plutenko <plutenkom@gmail.com>
2023-04-02 23:30:26 +0300

    persist install

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,63 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {contactsReduser} from "./contactsSlice";
-import {filterReduser} from "./filterSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { contactsReduser } from './contactsSlice';
+import { filterReduser } from './filterSlice';
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
+// const persistedcontactsReduser = persistReducer(persistConfig, contactsReduser);
+// const persistedfilterReduser = persistReducer(persistConfig, filterReduser);
+
+
+
+
 
 const store = configureStore({
-    reducer: {
-        contacts: contactsReduser,
-        filter: filterReduser,
-    }
+  reducer: {
+    contacts: contactsReduser,
+    filter: filterReduser,
+  },
 });
 
-export {store};
+
+// const store = configureStore({
+//   reducer: {
+//     contacts: persistReducer(persistConfig, contactsReduser),
+//     filter: filterReduser,
+//   },
+// });
+
+
+
+
+
+// const rootPersistConfig = {
+//   key: 'root',
+//   storage: storage,
+// };
+
+// const contactsPersistConfig = {
+//   key: 'contacts',
+//   storage: storage,
+// };
+
+// const rootReducer = combineReducers({
+//   contacts: persistReducer(contactsPersistConfig, contactsReduser),
+//   filter: filterReduser,
+// });
+
+// const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+// const store = createStore(persistedReducer);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
